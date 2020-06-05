e26f2d97ccda27fc8240bf4731f5bc0e3e3a8700
yusuke mitsugi <pastabal.jungle@gmail.com>
2020-06-05 22:32:16 +0900

    print("かきくけこ")を追記しました。

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         
         print("あいうえお")
         
+        print("かきくけこ")
+        
         // Do any additional setup after loading the view.
     }
 
